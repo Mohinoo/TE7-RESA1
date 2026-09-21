08d64d9635a3384c45ff214647066c4532bcc2bb
Mohamed <azimani.mo@gmail.com>
2026-09-21 18:06:28 +0200

    Add files via upload

--- sample-jalon1/client.c
+++ sample-jalon1/client.c
@@ -6,67 +6,110 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <poll.h>
 
 #include "common.h"
+#include "client.h"
 
 void echo_client(int sockfd) {
-	char buff[MSG_LEN];
-	int n;
-	while (1) {
-		// Cleaning memory
-		memset(buff, 0, MSG_LEN);
-		// Getting message from client
-		printf("Message: ");
-		n = 0;
-		while ((buff[n++] = getchar()) != '\n') {} // trailing '\n' will be sent
-		// Sending message (ECHO)
-		if (send(sockfd, buff, strlen(buff), 0) <= 0) {
-			break;
-		}
-		printf("Message sent!\n");
-		// Cleaning memory
-		memset(buff, 0, MSG_LEN);
-		// Receiving message
-		if (recv(sockfd, buff, MSG_LEN, 0) <= 0) {
-			break;
-		}
-		printf("Received: %s", buff);
-	}
+    struct pollfd fds[2];
+    char buff[MSG_LEN];
+    uint32_t net_size, size;
+    int n;
+
+    fds[0].fd = STDIN_FILENO;
+    fds[0].events = POLLIN;
+
+    fds[1].fd = sockfd;
+    fds[1].events = POLLIN;
+
+    printf("Connecté au serveur. Saisissez votre message (Tapez /quit pour quitter).\n");
+    
+    printf("Message: ");
+    fflush(stdout);
+
+    while (1) {
+        if (poll(fds, 2, -1) < 0) {
+            perror("poll()");
+            break;
+        }
+
+        // Message à envoyer
+        if (fds[0].revents & POLLIN) {
+            memset(buff, 0, MSG_LEN);
+            n = 0;
+            while ((buff[n] = getchar()) != '\n' && buff[n] != EOF) {
+                if (n < MSG_LEN - 1) n++;
+            }
+            buff[n] = '\0';
+
+            size = strlen(buff);
+            net_size = htonl(size);
+
+            if (send(sockfd, &net_size, sizeof(net_size), 0) <= 0) break;
+            if (send(sockfd, buff, size, 0) <= 0) break;
+
+            if (strcmp(buff, "/quit") == 0) {
+                printf("Déconnexion...\n");
+                break;
+            }
+            printf("Message sent!\n");
+        }
+
+        // Le serveur reçoit
+        if (fds[1].revents & POLLIN) {
+            n = recv(sockfd, &net_size, sizeof(net_size), 0);
+            if (n <= 0) {
+                printf("\nConnexion fermée par le serveur.\n");
+                break;
+            }
+            size = ntohl(net_size);
+
+            memset(buff, 0, MSG_LEN);
+            n = recv(sockfd, buff, size, 0);
+            if (n <= 0) break;
+            
+            printf("Received: %s\n", buff);
+
+            printf("Message: ");
+            fflush(stdout);
+        }
+    }
 }
 
-int handle_connect() {
-	struct addrinfo hints, *result, *rp;
-	int sfd;
-	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	if (getaddrinfo(SERV_ADDR, SERV_PORT, &hints, &result) != 0) {
-		perror("getaddrinfo()");
-		exit(EXIT_FAILURE);
-	}
-	for (rp = result; rp != NULL; rp = rp->ai_next) {
-		sfd = socket(rp->ai_family, rp->ai_socktype,rp->ai_protocol);
-		if (sfd == -1) {
-			continue;
-		}
-		if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1) {
-			break;
-		}
-		close(sfd);
-	}
-	if (rp == NULL) {
-		fprintf(stderr, "Could not connect\n");
-		exit(EXIT_FAILURE);
-	}
-	freeaddrinfo(result);
-	return sfd;
+int handle_connect(const char *server_name, const char *server_port) {
+    struct addrinfo hints, *result, *rp;
+    int sfd;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    
+    if (getaddrinfo(server_name, server_port, &hints, &result) != 0) {
+        perror("getaddrinfo()");
+        exit(EXIT_FAILURE);
+    }
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (sfd == -1) continue;
+        if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1) break;
+        close(sfd);
+    }
+    if (rp == NULL) {
+        fprintf(stderr, "Could not connect\n");
+        exit(EXIT_FAILURE);
+    }
+    freeaddrinfo(result);
+    return sfd;
 }
 
-int main() {
-	int sfd;
-	sfd = handle_connect();
-	echo_client(sfd);
-	close(sfd);
-	return EXIT_SUCCESS;
+int main(int argc, char *argv[]) {
+    int sfd;
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <server_name> <server_port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    sfd = handle_connect(argv[1], argv[2]);
+    echo_client(sfd);
+    close(sfd);
+    return EXIT_SUCCESS;
 }
-
--- sample-jalon1/server.c
+++ sample-jalon1/server.c
@@ -6,73 +6,161 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <poll.h>
 
 #include "common.h"
+#include "server.h"
 
-void echo_server(int sockfd) {
-	char buff[MSG_LEN];
-	while (1) {
-		// Cleaning memory
-		memset(buff, 0, MSG_LEN);
-		// Receiving message
-		if (recv(sockfd, buff, MSG_LEN, 0) <= 0) {
-			break;
-		}
-		printf("Received: %s", buff);
-		// Sending message (ECHO)
-		if (send(sockfd, buff, strlen(buff), 0) <= 0) {
-			break;
-		}
-		printf("Message sent!\n");
-	}
+#define MAX_CLIENTS 100
+
+struct client_node { // Liste chaînée
+    int fd;
+    char ip[INET_ADDRSTRLEN];
+    int port;
+    struct client_node *next;
+};
+
+struct client_node *clients_head = NULL;
+
+void add_client(int fd, struct sockaddr_in *addr) {
+    struct client_node *new_node = malloc(sizeof(struct client_node));
+    new_node->fd = fd;
+    inet_ntop(AF_INET, &(addr->sin_addr), new_node->ip, INET_ADDRSTRLEN);
+    new_node->port = ntohs(addr->sin_port);
+    new_node->next = clients_head;
+    clients_head = new_node;
+    printf("Nouveau client connecté : IP %s | Port %d | FD %d\n", new_node->ip, new_node->port, new_node->fd);
 }
 
-int handle_bind() {
-	struct addrinfo hints, *result, *rp;
-	int sfd;
-	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
-	if (getaddrinfo(NULL, SERV_PORT, &hints, &result) != 0) {
-		perror("getaddrinfo()");
-		exit(EXIT_FAILURE);
-	}
-	for (rp = result; rp != NULL; rp = rp->ai_next) {
-		sfd = socket(rp->ai_family, rp->ai_socktype,
-		rp->ai_protocol);
-		if (sfd == -1) {
-			continue;
-		}
-		if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0) {
-			break;
-		}
-		close(sfd);
-	}
-	if (rp == NULL) {
-		fprintf(stderr, "Could not bind\n");
-		exit(EXIT_FAILURE);
-	}
-	freeaddrinfo(result);
-	return sfd;
+void remove_client(int fd) {
+    struct client_node **curr = &clients_head;
+    while (*curr) {
+        if ((*curr)->fd == fd) {
+            struct client_node *to_delete = *curr;
+            *curr = (*curr)->next;
+            printf("Client déconnecté (FD %d).\n", fd);
+            free(to_delete);
+            return;
+        }
+        curr = &((*curr)->next);
+    }
 }
 
-int main() {
-	struct sockaddr cli;
-	int sfd, connfd;
-	socklen_t len;
-	sfd = handle_bind();
-	if ((listen(sfd, SOMAXCONN)) != 0) {
-		perror("listen()\n");
-		exit(EXIT_FAILURE);
-	}
-	len = sizeof(cli);
-	if ((connfd = accept(sfd, (struct sockaddr*) &cli, &len)) < 0) {
-		perror("accept()\n");
-		exit(EXIT_FAILURE);
-	}
-	echo_server(connfd);
-	close(sfd);
-	return EXIT_SUCCESS;
+void echo_server(int listen_sfd) {
+    struct pollfd fds[MAX_CLIENTS + 1];
+    int nfds = 1;
+
+    fds[0].fd = listen_sfd;
+    fds[0].events = POLLIN;
+
+    while (1) {
+        if (poll(fds, nfds, -1) < 0) {
+            perror("poll()");
+            break;
+        }
+
+        if (fds[0].revents & POLLIN) { //New connexion
+            struct sockaddr_in cli;
+            socklen_t len = sizeof(cli);
+            int connfd = accept(listen_sfd, (struct sockaddr *)&cli, &len);
+            if (connfd >= 0) {
+                if (nfds < MAX_CLIENTS + 1) {
+                    fds[nfds].fd = connfd;
+                    fds[nfds].events = POLLIN;
+                    nfds++;
+                    add_client(connfd, &cli);
+                } else {
+                    printf("Limite de clients atteinte. Rejet.\n"); //Si 100 clients
+                    close(connfd);
+                }
+            }
+        }
+
+        for (int i = 1; i < nfds; i++) {
+            if (fds[i].revents & POLLIN) {
+                int cfd = fds[i].fd;
+                char buff[MSG_LEN];
+                uint32_t net_size, size;
+
+                int n = recv(cfd, &net_size, sizeof(net_size), 0);
+                if (n <= 0) {
+                    close(cfd);
+                    remove_client(cfd);
+                    fds[i] = fds[nfds - 1];
+                    nfds--;
+                    i--;
+                    continue;
+                }
+
+                size = ntohl(net_size);
+                memset(buff, 0, MSG_LEN);
+                
+                n = recv(cfd, buff, size, 0);
+                
+                if (n <= 0 || strcmp(buff, "/quit") == 0) {
+                    close(cfd);
+                    remove_client(cfd);
+                    fds[i] = fds[nfds - 1];
+                    nfds--;
+                    i--;
+                } else {
+                    printf("Received (FD %d): %s\n", cfd, buff);
+                    // Renvoi au client expéditeur (ECHO)
+                    send(cfd, &net_size, sizeof(net_size), 0);
+                    send(cfd, buff, size, 0);
+                    printf("Message sent!\n");
+                }
+            }
+        }
+    }
 }
 
+int handle_bind(const char *server_port) {
+    struct addrinfo hints, *result, *rp;
+    int sfd;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    
+    if (getaddrinfo(NULL, server_port, &hints, &result) != 0) {
+        perror("getaddrinfo()");
+        exit(EXIT_FAILURE);
+    }
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (sfd == -1) continue;
+        int opt = 1;
+        setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+        if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0) break;
+        close(sfd);
+    }
+    if (rp == NULL) {
+        fprintf(stderr, "Could not bind\n");
+        exit(EXIT_FAILURE);
+    }
+    freeaddrinfo(result);
+    return sfd;
+}
+
+int main(int argc, char *argv[]) {
+    int sfd;
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <server_port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    
+    sfd = handle_bind(argv[1]);
+    
+    if ((listen(sfd, SOMAXCONN)) != 0) {
+        perror("listen()\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("Serveur en écoute sur le port %s...\n", argv[1]);
+    
+    echo_server(sfd);
+    
+    close(sfd);
+    return EXIT_SUCCESS;
+}
